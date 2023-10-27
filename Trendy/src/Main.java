634b2ffe03dfb7ae3e40f28f38b5d88e5bb8cda9
SurgingJungleMan <109383097+SurgingJungleMan@users.noreply.github.com>
2023-10-27 09:39:38 -0700

    The new updated trendy!

---
+++ Trendy/src/Main.java
@@ -0,0 +1,58 @@
+/*
+Tien Viet Doan
+CSA 2023
+10/25/23
+
+Get the total number of viewers per-second based off watch time and views count.
+
+
+ */
+
+import java.util.Scanner;
+
+
+public class Main {
+    Scanner mainScanner = new Scanner(System.in);
+
+    int totalViewers;
+    int totalMinutes;
+    int totalHours;
+    
+    String myInput;
+
+
+    public double getWatchTime(){
+        System.out.println("What is the total watch time in hours?");
+        myInput = mainScanner.nextLine();
+        totalHours = Integer.parseInt(myInput);
+
+        System.out.println("What is the total watch time in minutes?");
+        myInput = mainScanner.nextLine();
+        totalMinutes = Integer.parseInt(myInput);
+
+        double calcs = ((totalHours*60) + totalMinutes)*60; // total seconds
+
+        return calcs;
+    }
+
+    public double getRatio(double watchTime){
+        // total views * percentage of actual viewers / (hours/60 + minutes)/60
+
+        System.out.println("What is the total number of views?");
+        myInput = mainScanner.nextLine();
+        totalViewers = Integer.parseInt(myInput);
+
+        double calcs = totalViewers/watchTime;
+        calcs = (calcs+0.5);
+        calcs = (int)calcs;
+
+        return calcs;
+    }
+
+
+    public static void main(String[] args) {
+        double watchTime = new Main().getWatchTime();
+        double ratio = new Main().getRatio(watchTime);
+        System.out.println(ratio + " viewers were watching per second");
+    }
+}
